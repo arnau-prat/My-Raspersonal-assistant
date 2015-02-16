ef18e49595863d63ed1c8aefdd342fb9089aa696
Arnau Prat <arnauprat@icloud.com>
2015-02-16 22:51:57 +0000

    add tracker pwm

--- tracking-cam.py
+++ tracking-cam.py
@@ -5,28 +5,18 @@ import time
 
 DEBUG = True
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT)
-pwm = GPIO.PWM(18,100)
-pwm.start(0)
-pwm.ChangeDutyCycle(11)
 
-offSet =0
+
 
 faceCascade = cv2.CascadeClassifier("faces.xml")
 
 video_capture = cv2.VideoCapture(0)
 
-width = video_capture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
-height = video_capture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
-
-posMid = width/2
-
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    
+    width = 200
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -42,13 +32,10 @@ while True:
 
     	# Draw a rectangle around the faces
     	for (x, y, w, h) in faces:
-		
         	cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 		print "Position:("+str(x+w/2)+","+str(y+h/2)+")"
-		position = 90-60*((x+w/2-width/2)/width)
-		print "result: "+ str(position)
-		pwm.ChangeDutyCycle(position/10+2.5)
-		offSet = offSet + x - posMid
+		print str(width)
+		
     	# Display the resulting frame
     	cv2.imshow('Video', frame)
 
