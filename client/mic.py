b50bb4cba98ee9fd6789df538c162f0aa87c2cb4
Arnau Prat <arnau@vilynx.com>
2015-03-22 18:10:37 +0100

    code commented

--- client/mic.py
+++ client/mic.py
@@ -4,7 +4,8 @@ import math
 import wave
 import urllib  
 import urllib2 
-import stt
+import os
+
 
 class Microphone:
 
@@ -12,34 +13,31 @@ class Microphone:
         count = len(frame)/2
         format = "%dh"%(count)
         shorts = struct.unpack( format, frame )
-
         sum_squares = 0.0
         for sample in shorts:
             n = sample * (1.0/32768.0)
             sum_squares += n*n
         rms = math.pow(sum_squares/count,0.5);
-
         return rms * 1000
 
     def passiveListen(self,persona):
 
-        RATE = 16000
-        CHUNK = 1024
-        THRESHOLD =50
-        LISTEN_TIME = 3
+        CHUNK = 1024; RATE = 16000; THRESHOLD = 50; LISTEN_TIME = 3
 
-        all =[]
-
+        didDetect = False
+        
+        # prepare recording stream
         p = pyaudio.PyAudio()
-
         stream = p.open(format=pyaudio.paInt16,
                              channels=1,
                              rate=RATE,
                              input=True,
                              frames_per_buffer=CHUNK)
 
-        didDetect = False
+        # stores the audio data
+        all =[]
 
+        # starts passive listening for disturbances 
         for i in range(0, RATE / CHUNK * LISTEN_TIME):
             input = stream.read(CHUNK)
             rms_value = self.rms(input)
@@ -47,40 +45,39 @@ class Microphone:
                 didDetect = True
                 print "Listening...\n"
                 break
-        
+
         if not didDetect:
             stream.stop_stream()
             stream.close()
             return False
 
+        # append all the chunks
         all.append(input)
-
         for i in range(0, 10):
             data = stream.read(CHUNK)
             all.append(data)
 
+        # save the audio data   
         data = ''.join(all)
-
         stream.stop_stream()
         stream.close()
-
-        wf = wave.open('/Users/arnauprat/Dropbox/EI/src/12-33.wav', 'wb')
+        wf = wave.open('audio.wav', 'wb')
         wf.setnchannels(1)
         wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
         wf.setframerate(RATE)
         wf.writeframes(data)
         wf.close()
-
-        f = open('/Users/arnauprat/Dropbox/EI/src/12-33.wav')
-        audioFile = f.read()
-        f.close()
+        # check if persona was said
+        fp = open('audio.wav')
+        audioFile = fp.read()
+        fp.close()
         googl_speech_url = 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=AIzaSyCFAEJ8_7dR_9IfeJsTEEfbXQ5hZ0v8CxI'
         hrs = {'Content-type': 'audio/l16; rate=16000'}
         req = urllib2.Request(googl_speech_url, data=audioFile, headers=hrs)
-        p = urllib2.urlopen(req)
-        rawData = p.read()
+        rawData = urllib2.urlopen(req).read()
 
-        if "okay Google" in rawData:
+        if persona in rawData:
+            os.system ('afplay client/beep.mp3')
             return True
 
         return False
--- jarvis.py
+++ jarvis.py
@@ -4,25 +4,19 @@ from client import stt, tts, mic
 
 if __name__ == '__main__':
 
-	mic = mic.Microphone()
-
 	stt = stt.STTEngine()
 	tts = tts.TTSEngine()
+	mic = mic.Microphone()
 
 while True:
 
-	if mic.passiveListen("Ok Google"):
-
-		print "Say your command!!!!!!\n"
-
-		os.system ('afplay client/beep.mp3')
+	if mic.passiveListen("ok Google"):
 
 		os.system("sox -d voice.flac silence 1 0.1 5% 1 1.0 5%")
 
 		query = stt.transcript('voice.flac')
 
 		tts.say("You said: "+query)
-
 
 		print query
 
