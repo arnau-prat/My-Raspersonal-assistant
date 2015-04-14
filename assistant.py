1d27a6d14ef63805c9fcb96ee0159ff506fece8f
Arnau Prat <arnau@vilynx.com>
2015-04-14 09:11:37 +0200

    added spaces

--- assistant.py
+++ assistant.py
@@ -6,29 +6,25 @@ from client import stt, tts, mic
 
 if __name__ == '__main__':
 
-	stt = stt.STTEngine()
-	tts = tts.TTSEngine()
-	mic = mic.Microphone()
+    stt = stt.STTEngine()
+    tts = tts.TTSEngine()
+    mic = mic.Microphone()
 
-	wolfram_id='4X2W9X-AGA34L6QWQ'
-	client = wolframalpha.Client(wolfram_id)
+    wolfram_id='4X2W9X-AGA34L6QWQ'
+    client = wolframalpha.Client(wolfram_id)
 
 while True:
 
-	if mic.passiveListen("ok Google"):
-		os.system("sox -d voice.flac silence 1 0.1 5% 1 1.0 5%")
-		query = stt.transcript('voice.flac')
-		print query
-		res = client.query(query)
+    if mic.passiveListen("ok Google"):
+        os.system("sox -d voice.flac silence 1 0.1 5% 1 1.0 5%")
+        query = stt.transcript('voice.flac')
+        print query
+        res = client.query(query)
         if len(res.pods) > 0:
-        	pod = res.pods[1]
-        	tts.say(pod.text)
+            pod = res.pods[1]
+            tts.say(pod.text)
         else:
             tts.say("I have no answer for that")
-
-
-            
-
 
             
 
